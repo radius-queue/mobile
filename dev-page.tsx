e5fb90044629d18ca5022f0afbf2e9658e1b476c
wesleyplam <67298266+wesleyplam@users.noreply.github.com>
2020-07-22 17:03:39 -0700

    safeview for dev page

--- dev-page.tsx
+++ dev-page.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import {View, Button, StyleSheet} from 'react-native';
-import {useNavigation} from '@react-navigation/native';
+import React from "react";
+import { View, Button, StyleSheet, SafeAreaView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const DevPage = () => {
-
   const nav = useNavigation();
 
   return (
-    <View style={styles.container}>
-      <Button title={'Home'} onPress={() => nav.navigate('Home')}/>
-      <Button title={'Business Info'} onPress={() => nav.navigate('Business')}/>
-      <Button title={'Register'} onPress={() => nav.navigate('Register')}/>
-      <Button title={'Login'} onPress={() => nav.navigate('Login')}/>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Button title={"Home"} onPress={() => nav.navigate("Home")} />
+      <Button
+        title={"Business Info"}
+        onPress={() => nav.navigate("Business")}
+      />
+      <Button title={"Register"} onPress={() => nav.navigate("Register")} />
+      <Button title={"Login"} onPress={() => nav.navigate("Login")} />
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
+    width: "100%",
     flex: 1,
-  }
+  },
 });
 
 export default DevPage;
