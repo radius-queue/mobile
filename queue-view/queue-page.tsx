79aa349bd31ecb24c67a81688f3862e02d459e3f
grantwilliams <grantmw@uw.edu>
2020-07-25 21:20:35 -0700

    Line card structure and styling.

--- queue-view/queue-page.tsx
+++ queue-view/queue-page.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-
+import {Card, Text, Button, Layout} from '@ui-kitten/components';
+import {StyleSheet} from "react-native";
 import Screen from '../components/screen';
 import QueueList from './queue-list';
+import { ScrollView } from 'react-native-gesture-handler';
 
 /**
  * The page displaying relevant information regarding the user's
@@ -10,11 +11,35 @@ import QueueList from './queue-list';
  * @return {SafeAreaView} The entire page.
  */
 const QueuePage = () => {
+
   return (
     <Screen>
-      <QueueList />
+      <ScrollView style={styles.scroll}>
+        <Card style={styles.card}>
+          <Text style={styles.header}>Your Current Line</Text> 
+          <QueueList />
+          <Button style={styles.leaveButton} status='danger'>Leave Line</Button>
+        </Card>
+      </ScrollView>
     </Screen>
   );
 };
+
+const styles = StyleSheet.create({
+  card: {
+    marginHorizontal: 10,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  leaveButton: {
+  },
+  line: {
+    marginVertical: 10,
+  },
+  scroll: {
+  }
+});
 
 export default QueuePage;
