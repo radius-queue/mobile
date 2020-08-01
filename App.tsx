ad0e9c3aabea957ee41ddfd2e00e454dae873e63
wesleyplam <67298266+wesleyplam@users.noreply.github.com>
2020-07-31 17:41:31 -0700

    restyled nav bar

--- App.tsx
+++ App.tsx
@@ -1,47 +1,82 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+import { StyleSheet } from "react-native";
 
 import Me from "./profile/Me";
-import Login from "./profile/Login";
-import Register from "./profile/Register";
-import BusinessInfoScreen from "./business-info/business-info";
-import { BusinessInfo, User } from "./business-info/data";
 import DevPage from "./dev-page";
 import QueuePage from "./queue-view/queue-page";
 import ProfilePage from "./profile/profile-page";
 
-import { NavigationContainer, RouteProp } from "@react-navigation/native";
-import {
-  createBottomTabNavigator,
-  BottomTabNavigationProp,
-} from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
   ApplicationProvider,
   BottomNavigation,
   BottomNavigationTab,
+  IconRegistry,
+  Icon,
 } from "@ui-kitten/components";
 import * as eva from "@eva-design/eva";
 import { BusinessListScreen, businesses } from "./feed/feed";
 
-import { sampleUserInfo } from "./profile/profile-page";
-
-import { Entypo } from "@expo/vector-icons";
-import { MaterialIcons } from "@expo/vector-icons";
-import { Fontisto } from "@expo/vector-icons";
-import { AntDesign } from "@expo/vector-icons";
-
 import { default as theme } from "./custom-theme.json";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
 
 const Tab = createBottomTabNavigator();
 
-const feedIcon = <Entypo name="news" size={24} color="white" />;
-const meIcon = <MaterialIcons name="person" size={30} color="white" />;
-const queueIcon = <Fontisto name="list-1" size={22} color="white" />;
-const devIcon = <MaterialIcons name="developer-mode" size={24} color="white" />;
-const profileIcon = <AntDesign name="tags" size={30} color="white" />;
+<IconRegistry icons={EvaIconsPack} />;
+const devIcon = (props) => <Icon {...props} name="code-outline" />;
+
+const BottomTabBar = (Navigator: {
+  state: { index: number | undefined; routeNames: any[] };
+  navigation: { navigate: (arg0: any) => void };
+}) => (
+  <BottomNavigation
+    selectedIndex={Navigator.state.index}
+    onSelect={(index) =>
+      Navigator.navigation.navigate(Navigator.state.routeNames[index])
+    }
+  >
+    <BottomNavigationTab title="DEV" />
+    <BottomNavigationTab title="FEED" />
+    <BottomNavigationTab title="ME" />
+    <BottomNavigationTab title="QUEUE" />
+    <BottomNavigationTab title="PROFILE" />
+  </BottomNavigation>
+);
 
 const TabNavigator = () => (
+  <Tab.Navigator tabBar={(props) => <BottomTabBar {...props} />}>
+    <Tab.Screen name="Dev" component={DevPage} />
+    <Tab.Screen name="Feed">
+      {() => <BusinessListScreen {...businesses} />}
+    </Tab.Screen>
+    <Tab.Screen name="Me" component={Me} />
+    <Tab.Screen name="Queue" component={QueuePage} />
+    <Tab.Screen name="Profile" component={ProfilePage} />
+  </Tab.Navigator>
+);
+
+export default function App() {
+  <IconRegistry icons={EvaIconsPack} />;
+  return (
+    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </ApplicationProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+/* const TabNavigator = () => (
   <Tab.Navigator
     initialRouteName="Me"
     tabBarOptions={{
@@ -94,23 +129,4 @@ const TabNavigator = () => (
       {() => <ProfilePage {...sampleUserInfo} />}
     </Tab.Screen>
   </Tab.Navigator>
-);
-
-export default function App() {
-  return (
-    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-      <NavigationContainer>
-        <TabNavigator />
-      </NavigationContainer>
-    </ApplicationProvider>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+); */
---
+++ assets/Icons.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { Button, Icon } from "@ui-kitten/components";
+
+export const DevIcon = (props) => <Icon name="code-outline" {...props} />;
