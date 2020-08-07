4ea66fb13e184fa470fac6d3940ae41d041b5dc3
grantwilliams <grantmw@uw.edu>
2020-08-06 18:48:32 -0700

    Loads current firebase user for use in App.

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { StyleSheet } from "react-native";
 
 import Me from "./profile/Me";
@@ -20,6 +20,9 @@ import {
 import * as eva from "@eva-design/eva";
 import { default as theme } from "./custom-theme.json";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
+import {Customer} from "./util/customer"
+import firebase from "firebase";
+import {getCustomer} from "./util/api-functions";
 
 const Tab = createBottomTabNavigator();
 
@@ -50,7 +53,7 @@ const BottomTabBar = (Navigator: {
   </BottomNavigation>
 );
 
-const TabNavigator = () => (
+const TabNavigator = (user: Customer) => (
   <Tab.Navigator tabBar={(props) => <BottomTabBar {...props} />}>
     <Tab.Screen name="Dev" component={DevPage} />
     <Tab.Screen name="Feed">
@@ -59,10 +62,39 @@ const TabNavigator = () => (
     <Tab.Screen name="Me" component={Me} />
     <Tab.Screen name="Queue" component={QueuePage} />
     <Tab.Screen name="Profile">
-      {() => <ProfilePage {...sampleUserInfo} />}
+      {() => <ProfilePage {...user} />}
     </Tab.Screen>
   </Tab.Navigator>
 );
+
+const loggedInUser = firebase.auth().currentUser;
+let user: Customer;
+if (loggedInUser) {
+  console.log("yes");
+  getCustomer(loggedInUser.uid).then(function(userInfo) {
+    user = new Customer(
+      userInfo.firstName,
+      userInfo.lastName,
+      userInfo.email,
+      userInfo.phoneNumber,
+      userInfo.uid,
+      userInfo.currentQueue,
+      userInfo.favorites,
+      userInfo.recents,
+    );
+  }).catch(function(error) {
+    console.log("Unable to load user info:" + error);
+  });
+} else {
+  console.log("no");
+  user = new Customer(
+    '',
+    '',
+    '',
+    '',
+    '',
+  );
+}
 
 export default function App() {
   return (
@@ -70,7 +102,7 @@ export default function App() {
       <IconRegistry icons={EvaIconsPack} />
       <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
         <NavigationContainer>
-          <TabNavigator />
+          <TabNavigator {...user}/>
         </NavigationContainer>
       </ApplicationProvider>
     </>
@@ -85,58 +117,3 @@ const styles = StyleSheet.create({
     backgroundColor: theme['color-basic-1100'],
   },
 });
-
-/* const TabNavigator = () => (
-  <Tab.Navigator
-    initialRouteName="Me"
-    tabBarOptions={{
-      activeTintColor: "#DB8B00",
-      inactiveTintColor: "white",
-      activeBackgroundColor: "#091C7A",
-      inactiveBackgroundColor: "#091C7A",
-    }}
-  >
-    <Tab.Screen
-      name="Dev"
-      component={DevPage}
-      options={{
-        tabBarLabel: "DEV",
-        tabBarIcon: () => devIcon,
-      }}
-    />
-    <Tab.Screen
-      name="Feed"
-      options={{
-        tabBarLabel: "FEED",
-        tabBarIcon: () => feedIcon,
-      }}
-    >
-      {() => <BusinessListScreen {...businesses} />}
-    </Tab.Screen>
-    <Tab.Screen
-      name="Me"
-      component={Me}
-      options={{
-        tabBarLabel: "ME",
-        tabBarIcon: () => meIcon,
-      }}
-    />
-    <Tab.Screen
-      name="Queue"
-      component={QueuePage}
-      options={{
-        tabBarLabel: "QUEUE",
-        tabBarIcon: () => queueIcon,
-      }}
-    />
-    <Tab.Screen
-      name="Profile"
-      options={{
-        tabBarLabel: "PROFILE",
-        tabBarIcon: () => profileIcon,
-      }}
-    >
-      {() => <ProfilePage {...sampleUserInfo} />}
-    </Tab.Screen>
-  </Tab.Navigator>
-); */
