770a18610afc93c9c9f59006e87dd13b7b941068
wesleyplam <67298266+wesleyplam@users.noreply.github.com>
2020-08-09 21:52:06 -0700

    update tab style (caps)

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState, useRef, Dispatch, SetStateAction} from "react";
+import React, { useEffect, useState, useRef, Dispatch, SetStateAction } from "react";
 import { StyleSheet } from "react-native";
 
 import Me from "./profile/Me";
@@ -19,12 +19,12 @@ import {
 import * as eva from "@eva-design/eva";
 import { default as theme } from "./custom-theme.json";
 import { EvaIconsPack } from "@ui-kitten/eva-icons";
-import {Customer} from "./util/customer"
-import {getCustomer, getAllBusinessLocations, getBusinessLocationsFromArray} from "./util/api-functions";
-import { auth} from './firebase';
+import { Customer } from "./util/customer"
+import { getCustomer, getAllBusinessLocations, getBusinessLocationsFromArray } from "./util/api-functions";
+import { auth } from './firebase';
 import { Business } from "./util/business";
 
-const REGISTRATION_TIME_THRESHOLD : number = 3000;
+const REGISTRATION_TIME_THRESHOLD: number = 3000;
 
 const Tab = createBottomTabNavigator();
 
@@ -37,19 +37,19 @@ const BottomTabBar = (Navigator: {
   state: { index: number | undefined; routeNames: any[] };
   navigation: { navigate: (arg0: any) => void };
 }) => (
-  <BottomNavigation
-    selectedIndex={Navigator.state.index}
-    style={styles.bottomNavigation}
-    onSelect={(index) =>
-      Navigator.navigation.navigate(Navigator.state.routeNames[index])
-      
-    }
-  >
-    <BottomNavigationTab icon={FeedIcon} title="FEED" />
-    <BottomNavigationTab icon={MeIcon} title="Me" />
-    <BottomNavigationTab icon={QueueIcon} title="QUEUE" />
-  </BottomNavigation>
-);
+    <BottomNavigation
+      selectedIndex={Navigator.state.index}
+      style={styles.bottomNavigation}
+      onSelect={(index) =>
+        Navigator.navigation.navigate(Navigator.state.routeNames[index])
+
+      }
+    >
+      <BottomNavigationTab icon={FeedIcon} title="FEED" />
+      <BottomNavigationTab icon={MeIcon} title="ME" />
+      <BottomNavigationTab icon={QueueIcon} title="QUEUE" />
+    </BottomNavigation>
+  );
 
 interface TabProps {
   setUser: (c: Customer) => void,
@@ -66,25 +66,25 @@ export interface RenderProps {
   currUser: Customer,
 }
 
-const TabNavigator = ({setUser, currUser, setBusiness, business, setFavs, feedLists}: TabProps) => (
+const TabNavigator = ({ setUser, currUser, setBusiness, business, setFavs, feedLists }: TabProps) => (
   <Tab.Navigator tabBar={(props) => <BottomTabBar {...props} />}>
     <Tab.Screen name="Feed">
       {() => <BusinessListScreen
-          setBusiness={setBusiness}
-          setFavs={setFavs}
-          feedList={feedLists}
-          currUser={currUser}
-          business={business}
-        />}
+        setBusiness={setBusiness}
+        setFavs={setFavs}
+        feedList={feedLists}
+        currUser={currUser}
+        business={business}
+      />}
     </Tab.Screen>
     <Tab.Screen name="Me">
-      {() => <ProfileWrapper setUser={setUser} currUser={currUser}/>}
+      {() => <ProfileWrapper setUser={setUser} currUser={currUser} />}
     </Tab.Screen>
     <Tab.Screen name="Queue" component={QueuePage} />
   </Tab.Navigator>
 );
 
-const ProfileWrapper = ({setUser, currUser}: RenderProps) => (
+const ProfileWrapper = ({ setUser, currUser }: RenderProps) => (
   currUser.email.length > 0 ? <ProfilePage setUser={setUser} currUser={currUser} /> : <Me setUser={setUser} currUser={currUser} />
 );
 
@@ -97,7 +97,7 @@ export default function App() {
   const [business, setBusiness] = useState<[BusinessLocation | undefined, number]>([undefined, 0]); // the business you're queueing in
 
   useEffect(() => {
-    const unsub = auth.onAuthStateChanged(async function(user) {
+    const unsub = auth.onAuthStateChanged(async function (user) {
       if (user) {
         let customer = currUser;
         if (new Date().getTime() - new Date(user.metadata.creationTime!).getTime() > REGISTRATION_TIME_THRESHOLD) {
@@ -126,7 +126,7 @@ export default function App() {
 
     return unsub;
   }, []);
-  
+
   return (
     <>
       <IconRegistry icons={EvaIconsPack} />
